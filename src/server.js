540c05fd77257e2721fd6f3a41c83c19588602b9
Josephine Davison <88828544+josiedavison@users.noreply.github.com>
2022-10-19 16:50:47 +1000

    Update server.js

--- src/server.js
+++ src/server.js
@@ -41,10 +41,8 @@ const fetch = (...args) =>
 //import redis
 const redis = require('redis');
 const redisClient = redis.createClient(6379,'redis');
-redisClient.on('error', (err) => {
-    console.log('Error occured while connecting or accessing redis server');
-});
-if(!redisClient.get('customer_name',redis.print)) {
+client.connect().then(() => {
+  if(!redisClient.get('customer_name',redis.print)) {
     //create a new record
     redisClient.set('customer_name','John Doe', redis.print);
     console.log('Writing Property : customer_name');
@@ -53,6 +51,10 @@ if(!redisClient.get('customer_name',redis.print)) {
     console.log(`Reading property : customer_name - ${val}`);
 }
 
+})
+redisClient.on('error', (err) => {
+    console.log('Error occured while connecting or accessing redis server');
+});
 
 
 const { json } = require('express');
