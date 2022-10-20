ccb9561d6c76fa8e8c48198c1d457666f1457185
Josephine Davison <88828544+josiedavison@users.noreply.github.com>
2022-10-20 18:33:21 +1000

    added redis

--- src/server.js
+++ src/server.js
@@ -14,6 +14,19 @@ const PLAYLIST2 = "/playlists";
 const ADDSONGS = "https://api.spotify.com/v1/playlists/";
 const ADDSONGS2 = "/tracks?uris=";
 
+const redis = require('redis');
+const redisClient = redis.createClient('redis://cache');
+redisClient.on('error', (err) => {
+    console.log('Error occured while connecting or accessing redis server');
+});
+if(!redisClient.get('customer_name',redis.print)) {
+    //create a new record
+    redisClient.set('customer_name','John Doe', redis.print);
+    console.log('Writing Property : customer_name');
+} else {
+    let val = redisClient.get('customer_name',redis.print);
+    console.log(`Reading property : customer_name - ${val}`);
+}
 
 
 
